3abbd72f6dd2d2d450bc12ec9fec07c6575db7bb
Yesteb <estebansanchez474@outlook.com>
2025-05-18 09:33:02 -0500

    Version: 0.1.0

---
+++ 06Code/0.1.0/src/ec/espe/edu/jsnow/model/Login.java
@@ -0,0 +1,30 @@
+
+package ec.espe.edu.jsnow.model;
+import java.util.Scanner;
+/**
+ *
+ * @author yesteb
+ */
+public class Login {
+    
+    public static void login(){
+    Scanner scanner = new Scanner(System.in);
+    String username;
+    String password;    
+    
+        System.out.print("Username: ");
+        username = scanner.nextLine();
+        System.out.print("Password: ");
+        
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
