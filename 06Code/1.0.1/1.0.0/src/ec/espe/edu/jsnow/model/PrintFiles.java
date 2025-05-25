56eabfc0b7513bd6f2c162bd429e55d98a813631
Yesteb <estebansanchez474@outlook.com>
2025-05-24 22:36:40 -0500

    Version: 1.0.1 Description: Optimizacion de programa

---
+++ 06Code/1.0.1/1.0.0/src/ec/espe/edu/jsnow/model/PrintFiles.java
@@ -0,0 +1,27 @@
+
+package ec.espe.edu.jsnow.model;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+/**
+ *
+ * @author Yesteb JSnow
+ */
+public class PrintFiles {
+    
+    public static void printMenu(String directoryPath) throws FileNotFoundException, IOException{
+        BufferedReader bufferedReader;
+        String lineFile;
+        
+        bufferedReader = new BufferedReader(new FileReader(directoryPath));
+        while((lineFile = bufferedReader.readLine()) != null){
+            System.out.println(lineFile);
+        }
+        bufferedReader.close();
+    }
+    
+    public static void printProductList(String directoryPath){
+        
+    }
+}
