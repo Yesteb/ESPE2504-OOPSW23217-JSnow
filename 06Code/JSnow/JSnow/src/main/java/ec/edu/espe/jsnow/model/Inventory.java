0d11e057ca56670e9f9661f93d1a70bee60b77cd
Yesteb <estebansanchez474@outlook.com>
2025-06-29 08:54:56 -0500

    Version: 0.0.1 Description:Change command aplication to maden

---
+++ 06Code/JSnow/JSnow/src/main/java/ec/edu/espe/jsnow/model/Inventory.java
@@ -0,0 +1,29 @@
+
+package ec.edu.espe.jsnow.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Yesteb JSnow
+ */
+public class Inventory {
+    ArrayList<Product> product;
+    
+    public static void addProduct(Product product){
+        
+    } 
+    public static void removeProduct(String name){
+        
+    }
+    public static void getLowStock(){
+        
+    }
+    public static void generateInventoryReport (){
+        
+        
+        
+        
+    }
+    
+}
