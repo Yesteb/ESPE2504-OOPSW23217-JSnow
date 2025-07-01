6b8518f9a468b68d1d14ed870576b88dcde97951
Jahsivi <kjsivinta@espe.edu.ec>
2025-06-30 23:53:56 -0500

    Splash ScreemGUIv2

---
+++ 06Code/JSnowVs.0.3.2/JSnow/src/main/java/ec/edu/espe/jsnow/model/Admin.java
@@ -0,0 +1,19 @@
+
+package ec.edu.espe.jsnow.model;
+
+/**
+ *
+ * @author Yesteb JSnow
+ */
+public class Admin extends User{
+
+    public Admin(String username, String password) {
+        super(username, password);
+    }
+
+    @Override
+    public void showDashboard() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
