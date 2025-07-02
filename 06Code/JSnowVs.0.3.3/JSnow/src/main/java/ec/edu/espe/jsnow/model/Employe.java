b484579e9a5518db6abc85f8b148052c403f46ac
Jahsivi <kjsivinta@espe.edu.ec>
2025-07-01 22:45:20 -0500

    Menu InterfazV0.3.3

---
+++ 06Code/JSnowVs.0.3.3/JSnow/src/main/java/ec/edu/espe/jsnow/model/Employe.java
@@ -0,0 +1,19 @@
+
+package ec.edu.espe.jsnow.model;
+
+/**
+ *
+ * @author Yesteb JSnow
+ */
+public class Employe extends User{
+
+    public Employe(String username, String password) {
+        super(username, password);
+    }
+
+    @Override
+    public void showDashboard() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
